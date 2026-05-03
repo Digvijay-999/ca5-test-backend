d8bcdc1b00499600bd5b76401de03fde02cd3cd2
Digvijay <dypatil2007@gmail.com>
2026-05-04 01:31:32 +0530

    fix port for render

--- index.js
+++ index.js
@@ -25,9 +25,11 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
 
-    app.listen(process.env.PORT, () => {
-      console.log(`🚀 Server running on port ${process.env.PORT}`);
-    });
+    const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
   })
   .catch(err => {
